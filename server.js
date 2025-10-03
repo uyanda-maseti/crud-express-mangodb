0dc491b39d5692951d42622a3d11280f2c1a3f12
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2025-10-03 13:31:21 +0000

    Create form ...

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ app.listen(3000, function () {
     console.log('Listening on 3000')
 })
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', function (req, res) {
     res.sendFile('/workspaces/crud-express-mangodb' + '/index.html')
+})
+app.post('/quotes', (req, res) => {
+    console.log('Hellooooooo!')
 })
